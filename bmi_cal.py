99e04c14661026f71f12e8f2da0d72c526b25dc1
Anusha988 <bantuanusha20@gmail.com>
2020-04-14 13:47:28 +0530

    added file

---
+++ bmi_cal.py
@@ -0,0 +1,7 @@
+#Assignment-1
+#Name:Adult body mass index calculater
+w=float(input("enter your weight in kilogram:"))
+h=float(input("enter your height in meters:"))
+cal=w/h
+result=cal/h
+print(result)
